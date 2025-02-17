e4474f7464f10f8cea8bdbc8a72b2badaa97b235
unknown <jacqui.longhurst@btinternet.com>
2025-02-17 17:43:10 +0100

    switch to next.js, add docs app and update eslint version

--- packages/ui/tailwind.config.ts
+++ packages/ui/tailwind.config.ts
@@ -2,10 +2,7 @@ import type { Config } from "tailwindcss";
 import sharedConfig from "@repo/tailwind-config";
 
 const config: Pick<Config, "prefix" | "presets" | "content"> = {
-  content: [
-    "./components/**/*.{js,ts,jsx,tsx}",
-    "./components/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./src/**/*.tsx"],
   prefix: "ui-",
   presets: [sharedConfig],
 };
